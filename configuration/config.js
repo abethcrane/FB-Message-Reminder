3e2a0986d3c17b02fe02000df1fca80cf03e6de9
beth <abethcrane@gmail.com>
2015-06-08 23:04:16 -0700

    Super basic Facebook login tutorial
    lol casual fbsecret on github nbd

---
+++ configuration/config.js
@@ -0,0 +1,16 @@
+module.exports={
+/*
+* This file contains the configurations information of Twitter login app.
+* It consists of Twitter app information, database information.
+*/
+
+	"facebook_client_id" 	: 				"421145674734383",
+	"facebook_api_secret"	:				"f231db11fe3589149ca5d86b76469ffc",
+	"callback_url"			:				"http://localhost:1337/auth/facebook/callback",
+	"use_database"			:				"false",
+	"host"					:				"localhost",
+	"username"				:				"root",
+	"password"				:				"",
+	"database"				:				"",
+	"cookie_secret"			:				"correct horse battery staple"
+}
